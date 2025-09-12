f5a36cae5b3ded2c7c4b0d1a7823d14dfaa7fe99
alexandralesan1 <alexandra.lesan@isa.utm.md>
2025-09-12 22:46:45 +0300

    First commit

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t border-gray-200">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          {" "}
+          &copy; 2025 FakeStoreName, Inc. All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
